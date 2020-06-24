c3b1e2a9da02db57468ae9108868221186437ee5
kamwithak <kamran.choudhry@mail.mcgill.ca>
2020-06-24 15:13:32 -0400

    final edits for the day

--- programLauncher.py
+++ programLauncher.py
@@ -4,19 +4,19 @@ class programLauncher:
     def __init__(self, text):
         self.text = text
         self.actionLabels = ["launch", "start", "open", "begin", "kick off", "commence", "turn on", "give me", "instantiate", "initialize"]
-        self.programLables = self.populateProgramLabels()
+        self.programLables = self.getLocallyInstalledProgramNames()
         # ~
         #self.launch(self.text)
 
-    def populateProgramLabels(self):
+    def getLocallyInstalledProgramNames(self):
         return [i.name for i in self.findAllLocalPrograms()]
         
     def findAllLocalPrograms(self):
         return [app for app in winapps.list_installed()]
-        
+
     def launch(self, text):
         for action in self.actionLabels:
-            for program in self.programLabels:
+            for program in self.programLables:
                 if (action in text and program in text):
                     os.startfile(r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
                     return 
