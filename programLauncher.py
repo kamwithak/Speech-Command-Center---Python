940a7b5ad3684a29a01e5686870f3aed68644896
kamwithak <kamran.choudhry@mail.mcgill.ca>
2020-07-05 23:49:30 -0400

    edit

--- programLauncher.py
+++ programLauncher.py
@@ -3,11 +3,22 @@ import os, winapps
 class programLauncher:
     def __init__(self, text):
         self.text = text
-        self.actionLabels = ["launch", "start", "open", "begin", "kick off", "commence", "turn on", "give me", "instantiate", "initialize"]
+        self.actionLabels = [
+            "launch",
+            "start",
+            "open",
+            "begin",
+            "kick off",
+            "commence",
+            "turn on",
+            "give me",
+            "instantiate",
+            "initialize"
+        ]
         self.programDict = self.getInstalledPrograms()
-
+        # ~~~~~~~~~~~~~~~~~~~~~
         print(self.programDict)
-        # ~
+        # ~~~~~~~~~~~~~~~~~~~~~
         self.launch(self.text)
 
     '''
